a85196420966b8941934ea85d0d0686dd614c31a
flutter-jihoon <lim.jihoon@cashwalk.io>
2024-06-28 11:06:00 +0900

    Add UiKitview iOS측 구현

--- ios/Runner/TestNativeView2.swift
+++ ios/Runner/TestNativeView2.swift
@@ -1,14 +1,14 @@
 //
-//  FLNativeView.swift
+//  TestNativeView2.swift
 //  Runner
 //
-//  Created by 임지훈 on 6/27/24.
+//  Created by 임지훈 on 6/28/24.
 //
 
 import Flutter
 import UIKit
 
-class FLNativeViewFactory: NSObject, FlutterPlatformViewFactory {
+class TestNativeViewFactory2: NSObject, FlutterPlatformViewFactory {
     private var messenger: FlutterBinaryMessenger
 
     init(messenger: FlutterBinaryMessenger) {
@@ -21,7 +21,7 @@ class FLNativeViewFactory: NSObject, FlutterPlatformViewFactory {
         viewIdentifier viewId: Int64,
         arguments args: Any?
     ) -> FlutterPlatformView {
-        return FLNativeView(
+        return TestNativeView2(
             frame: frame,
             viewIdentifier: viewId,
             arguments: args,
@@ -34,9 +34,9 @@ class FLNativeViewFactory: NSObject, FlutterPlatformViewFactory {
     }
 }
 
-class FLNativeView: NSObject, FlutterPlatformView {
+class TestNativeView2: NSObject, FlutterPlatformView {
     private var _view: UIView
-
+    
     init(
         frame: CGRect,
         viewIdentifier viewId: Int64,
@@ -48,18 +48,30 @@ class FLNativeView: NSObject, FlutterPlatformView {
         // iOS views can be created here
         createNativeView(view: _view)
     }
-
+    
     func view() -> UIView {
         return _view
     }
-
+    
     func createNativeView(view _view: UIView){
-        _view.backgroundColor = UIColor.blue
+        _view.backgroundColor = UIColor.lightGray
         let nativeLabel = UILabel()
-        nativeLabel.text = "Native text from iOS"
+        nativeLabel.text = "리스트 네이티브 뷰"
         nativeLabel.textColor = UIColor.white
         nativeLabel.textAlignment = .center
         nativeLabel.frame = CGRect(x: 0, y: 0, width: 180, height: 48.0)
         _view.addSubview(nativeLabel)
+        
+        // 뷰에 탭 제스처 추가
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        _view.addGestureRecognizer(tapGestureRecognizer)
+        _view.isUserInteractionEnabled = true
+    }
+    
+    @objc func viewTapped() {
+        if let url = URL(string: "https://www.example.com") {
+            UIApplication.shared.open(url)
+        }
     }
 }
+
