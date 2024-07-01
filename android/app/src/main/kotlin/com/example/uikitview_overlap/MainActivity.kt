d6a48e79f1c537fbf88c67e337b4bb4e62a3d385
flutter-jihoon <lim.jihoon@cashwalk.io>
2024-07-01 16:49:19 +0900

    Refactor Android 클래스별 파일 분리

--- android/app/src/main/kotlin/com/example/uikitview_overlap/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/uikitview_overlap/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.uikitview_overlap
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine
+                .platformViewsController
+                .registry
+                .registerViewFactory("test1", 
+                                      TestNativeViewFactory1())
+        flutterEngine
+                .platformViewsController
+                .registry
+                .registerViewFactory("test2", 
+                                      TestNativeViewFactory2())
+    }
 }
---
+++ android/app/src/main/kotlin/com/example/uikitview_overlap/TestNativeViewFactory2.kt
@@ -0,0 +1,13 @@
+package com.example.uikitview_overlap
+
+import android.content.Context
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+
+class TestNativeViewFactory2 : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+        val creationParams = args as Map<String?, Any?>?
+        return TestNativeView2(context, viewId, creationParams)
+    }
+}
